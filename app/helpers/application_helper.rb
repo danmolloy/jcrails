48f42f325f6895f48c6fccac61ffe148b7cabba7
danmolloy <danieljmolloy1@gmail.com>
2016-02-02 20:32:02 +0000

    add page title and description helper methods to application layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+  def full_title(page_title)
+    base_title = "JobContax"
+    if page_title.empty?
+      base_title
+    else
+      "#{base_title} | #{page_title}"
+    end
+  end
+
+  def page_description(description)
+    if description.empty?
+      "JobContax - Technical Recruitment Specialists"
+    else
+      description
+    end
+  end
 end
